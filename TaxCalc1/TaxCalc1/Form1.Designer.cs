37404487002e6ffde047592eb9030768c6429a10
kuonflower <68059211+kuonflower@users.noreply.github.com>
2020-10-22 21:49:25 +0900

    Add TaxCalc 8% 10%

---
+++ TaxCalc1/TaxCalc1/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace TaxCalc1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.priceBox = new System.Windows.Forms.TextBox();
+            this.taxPriceBox = new System.Windows.Forms.TextBox();
+            this.calcButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // priceBox
+            // 
+            this.priceBox.Location = new System.Drawing.Point(211, 49);
+            this.priceBox.Name = "priceBox";
+            this.priceBox.Size = new System.Drawing.Size(100, 19);
+            this.priceBox.TabIndex = 0;
+            // 
+            // taxPriceBox
+            // 
+            this.taxPriceBox.Enabled = false;
+            this.taxPriceBox.Location = new System.Drawing.Point(211, 106);
+            this.taxPriceBox.Name = "taxPriceBox";
+            this.taxPriceBox.Size = new System.Drawing.Size(100, 19);
+            this.taxPriceBox.TabIndex = 1;
+            // 
+            // calcButton
+            // 
+            this.calcButton.Location = new System.Drawing.Point(211, 182);
+            this.calcButton.Name = "calcButton";
+            this.calcButton.Size = new System.Drawing.Size(100, 23);
+            this.calcButton.TabIndex = 2;
+            this.calcButton.Text = "計算！！10％！";
+            this.calcButton.UseVisualStyleBackColor = true;
+            this.calcButton.Click += new System.EventHandler(this.CalcButtonClicked);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(93, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "税抜価格";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(93, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "税込価格10％";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 270);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.calcButton);
+            this.Controls.Add(this.taxPriceBox);
+            this.Controls.Add(this.priceBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox priceBox;
+        private System.Windows.Forms.TextBox taxPriceBox;
+        private System.Windows.Forms.Button calcButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ TaxCalc1/TaxCalc1/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaxCalc1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CalcButtonClicked(object sender, EventArgs e)
+        {
+            int price ;
+            bool success = int.TryParse(this.priceBox.Text, out price);
+
+            if (success)
+            {
+                int taxPrice = (int)(price * 1.1);
+                this.taxPriceBox.Text = taxPrice.ToString();
+            }
+            else
+            {
+                MessageBox.Show("税抜き価格を正しく入力してください");
+            }
+        }
+    }
+}
